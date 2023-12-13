ba2a7ffa6390c751c50af44332aeec658909b99a
Brett Taylor <bretttaylor1998@live.co.uk>
2023-12-13 21:53:46 +0000

    Added unarmed attacks to Player Combat Script

--- engine-internal/src/main/java/com/terryscape/game/combat/script/PlayerCombatScript.java
+++ engine-internal/src/main/java/com/terryscape/game/combat/script/PlayerCombatScript.java
@@ -1,6 +1,5 @@
 package com.terryscape.game.combat.script;
 
-import com.terryscape.cache.item.ItemDefinition;
 import com.terryscape.game.combat.CombatComponent;
 import com.terryscape.game.combat.CombatScript;
 import com.terryscape.game.combat.health.DamageInformation;
@@ -10,7 +9,10 @@ import com.terryscape.game.equipment.EquipmentSlot;
 import com.terryscape.game.movement.AnimationComponent;
 import com.terryscape.game.movement.MovementComponent;
 import com.terryscape.game.player.PlayerComponent;
+import com.terryscape.maths.RandomUtil;
 import com.terryscape.world.WorldClock;
+
+import java.util.List;
 
 public class PlayerCombatScript implements CombatScript {
 
@@ -48,10 +50,39 @@ public class PlayerCombatScript implements CombatScript {
         }
 
         var mainHand = playerComponent.getEquipment().getSlot(EquipmentSlot.MAIN_HAND);
+        var offHand = playerComponent.getEquipment().getSlot(EquipmentSlot.OFF_HAND);
+
+        if (mainHand.isPresent() || offHand.isPresent()) {
+            return handleAttackWithWeapon(victim);
+        }
+
+        slap(victim, true, pickUnarmedAttackAnimationId());
+        return true;
+    }
+
+    private String pickUnarmedAttackAnimationId() {
+        var animations = List.of(
+            "Unarmed_Attack_Kick_L1",
+            "Unarmed_Attack_Kick_L2",
+            "Unarmed_Attack_Kick_R1",
+            "Unarmed_Attack_Kick_R2",
+            "Unarmed_Attack_L1",
+            "Unarmed_Attack_L2",
+            "Unarmed_Attack_L3",
+            "Unarmed_Attack_R1",
+            "Unarmed_Attack_R2",
+            "Unarmed_Attack_R3"
+        );
+
+        return RandomUtil.randomCollection(animations);
+    }
+
+    private boolean handleAttackWithWeapon(CombatComponent victim) {
+        var mainHand = playerComponent.getEquipment().getSlot(EquipmentSlot.MAIN_HAND);
         var isMainHandOffCooldown = lastMainHandAttackTime + 6 < worldClock.getNowTick();
 
         if (mainHand.isPresent() && isMainHandOffCooldown) {
-            slap(victim, true, mainHand.get());
+            slap(victim, true, mainHand.get().getAnimationMainHandAttack());
             return true;
         }
 
@@ -59,25 +90,24 @@ public class PlayerCombatScript implements CombatScript {
         var isOffHandOffCooldown = lastOffHandAttackTime + 6 < worldClock.getNowTick();
 
         if (offHand.isPresent() && isOffHandOffCooldown) {
-            slap(victim, false, offHand.get());
+            slap(victim, false, offHand.get().getAnimationOffHandAttack());
             return true;
         }
 
         return false;
     }
 
-    private void slap(CombatComponent victim, boolean mainHand, ItemDefinition item) {
+    private void slap(CombatComponent victim, boolean mainHand, String animationIdToPlay) {
         lastAttackTime = worldClock.getNowTick();
 
         var damage = new DamageInformation().setAmount(1);
+        animationComponent.playAnimation(animationIdToPlay);
 
         if (mainHand) {
             lastMainHandAttackTime = worldClock.getNowTick();
-            animationComponent.playAnimation(item.getAnimationMainHandAttack());
             damage.setType(DamageType.MELEE_MAIN_HAND);
         } else {
             lastOffHandAttackTime = worldClock.getNowTick();
-            animationComponent.playAnimation(item.getAnimationOffHandAttack());
             damage.setType(DamageType.MELEE_OFF_HAND);
         }
 
